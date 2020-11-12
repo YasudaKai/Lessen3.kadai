1a794a5975f5449389d5801a91467b196b5604e8
kai.yasuda <kai0603603@gmail.com>
2020-11-12 13:47:00 +0900

    課題提出　1回目

--- app/src/main/java/jp/techacademy/kai/yasuda/MainActivity.kt
+++ app/src/main/java/jp/techacademy/kai/yasuda/MainActivity.kt
@@ -8,5 +8,14 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val human1 = Human("太郎", 20, "サッカー")
+        val human2 = Human("次郎", 10, "野球")
+
+        human1.think()
+        human1.say()
+        human2.think()
+        human2.say()
+
     }
 }
---
+++ app/src/main/java/jp/techacademy/kai/yasuda/Thinkable.kt
@@ -0,0 +1,5 @@
+package jp.techacademy.kai.yasuda
+
+interface Thinkable {
+    fun think()
+}
